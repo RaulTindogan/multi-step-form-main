c42362103c5b5eb5b76dd45c11eab43af755f973
RaulTindogan <tindoganraul@gmail.com>
2024-05-07 23:30:36 +0800

    Created the basic logic of summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
-function Summary() {
+function Summary({planState, setplanState, addOns, setAddOns}) {
+  console.log(addOns)
   return (
-    <div>Summary</div>
+    <div>
+      <div>
+        <h1>Finishing Up</h1>
+        <p>Double-check everything looks OK beform confirming</p>
+      </div>
+      <div>
+        {
+          <div>    
+            <p>
+              {planState.planName} (Monthly)
+              <span>Change</span>
+            </p>
+            <p>{planState.price}</p>     
+          </div>
+        }       
+        {
+          addOns.map(addOn=>(
+            <div key={addOn.id}>
+              <p>
+                {addOn.addOnName}
+                <span>{addOn.addOnPrice}</span>
+              </p>
+            </div>
+          ))
+        }
+        
+        {/* <p>
+          Total (per month):
+          <span>+12/mo</span>
+        </p> */}
+      </div>
+    </div>
   )
 }
 
